fc8bb51e83204ea6c370d622cd437faf57ffb72f
KulondwaLandry <landrymufundi@gmail.com>
2023-08-19 07:40:41 -0400

    get user by id

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,8 +3,19 @@ const router = express.Router();
 const uuid = require("uuid");
 let users = require("../../Users");
 
+//get all users
 router.get("/", (req, res) => {
   res.json(users);
+});
+
+// get user by ID
+router.get("/:id", (req, res) => {
+  const found = users.some((user) => user.id === parseInt(req.params.id));
+  if (found) {
+    res.json(users.filter((user) => user.id === parseInt(req.params.id)));
+  } else {
+    res.sendStatus(400);
+  }
 });
 
 module.exports = router;
