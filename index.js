55003c9280fd3697e08e3d14a709b2a1874b90c9
KulondwaLandry <landrymufundi@gmail.com>
2023-08-22 12:57:25 -0400

    mongoose

--- index.js
+++ index.js
@@ -1,23 +1,33 @@
-const bodyParser = require("body-parser");
 const express = require("express");
-const app = express();
+var app = express();
 const port = 8000;
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-// api for user
-app.use("/api/users", require("./routes/api/users"));
+const handlebars = require("handlebars");
+const path = require("path");
+const exphbs = require("express-handlebars");
+const {
+  allowInscurePrototypeAccess,
+} = require("@handlebars/allow-prototype-access");
+const bodyparser = require("body-parser");
 
-//api for movies
-app.use("/api/movies", require("./routes/api/movies"));
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
 
-// jwt api
-app.use("/api/authentification", require("./routes/api/jwt"));
+app.get("/", (req, res) => {
+  res.send("<p>hello world !</p>");
+});
 
-// mongodb usage api
-app.use("/api/mongo/", require("./routes/api/mongo"));
+app.set("views", path.join(__dirname, "/views/"));
+app.engine(
+  "hbs",
+  exphbs({
+    handlebars: allowInscurePrototypeAccess,
+    extname: "hbs",
+    defaultLayout: "MainLayout",
+    layoutsDir: __dirname + "/views/layouts",
+  })
+);
+app.set("view engine", "hbs");
 
 app.listen(port, () => {
-  console.log(`the server listens to the port ${port}`);
+  console.log(`the server is listening on the port ${port}`);
 });
--- test.js
+++ test.js
@@ -9,3 +9,27 @@
 // app.listen(3000, () => {
 //   console.log("server runnning on port 3000");
 // });
+
+const bodyParser = require("body-parser");
+// const express = require("express");
+// const app = express();
+// const port = 8000;
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+// // api for user
+// app.use("/api/users", require("./routes/api/users"));
+
+// //api for movies
+// app.use("/api/movies", require("./routes/api/movies"));
+
+// // jwt api
+// app.use("/api/authentification", require("./routes/api/jwt"));
+
+// // mongodb usage api
+// app.use("/api/mongo/", require("./routes/api/mongo"));
+
+// app.listen(port, () => {
+//   console.log(`the server listens to the port ${port}`);
+// });
