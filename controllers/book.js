2acb6e98b1f1b477cc17724669bf79bb40097670
KulondwaLandry <landrymufundi@gmail.com>
2023-08-28 10:05:25 -0400

    joi implementation

--- controllers/book.js
+++ controllers/book.js
@@ -5,7 +5,7 @@ exports.createBook = async (req, res) => {
   try {
     const book = await (
       await booksServices.createBook(req.body)
-    ).populate("author", { strictPopulate: false });
+    ).populate("author");
     res.json({ data: book, status: "success" });
   } catch (err) {
     res.status(500).json({ message: err.message });
